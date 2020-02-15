2645e82649f137917d80699d28c2d4e051828dba
dinger22 <dingyinghao@hotmail.com>
2020-02-14 22:41:33 -0500

    use factory pattern to do IoC

--- src/main/java/com/ioc/example/customer/CustomerBL.java
+++ src/main/java/com/ioc/example/customer/CustomerBL.java
@@ -1,27 +1,29 @@
 package com.ioc.example.customer;
 
-public class CustomerBL
-{
-    DataAccess _dataAccess;
+public class CustomerBL {
 
-    public CustomerBL()
-    {
-        _dataAccess = new DataAccess();
+    public CustomerBL() {
+
     }
 
-    public String GetCustomerName(int id)
-    {
+    public String GetCustomerName(int id) {
+        DataAccess _dataAccess = DataAccessFactory.GetDataAccess();
         return _dataAccess.GetCustomerName(id);
     }
 }
 
-class DataAccess //would be a new file in real app
+class DataAccess // would be a new file in real app
 {
-    public DataAccess()
-    {
+    public DataAccess() {
     }
 
     public String GetCustomerName(int id) {
         return "Dummy Customer Name"; // get it from DB in real app
+    }
+}
+
+class DataAccessFactory {
+    public static DataAccess GetDataAccess() {
+        return new DataAccess();
     }
 }
