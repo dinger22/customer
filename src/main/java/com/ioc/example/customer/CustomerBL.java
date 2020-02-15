1ecde26f7ea18796351caa8148f0bfd7133f41cb
dinger22 <dingyinghao@hotmail.com>
2020-02-14 22:00:03 -0500

    Init commit, tightly coupled

---
+++ src/main/java/com/ioc/example/customer/CustomerBL.java
@@ -0,0 +1,27 @@
+package com.ioc.example.customer;
+
+public class CustomerBL
+{
+    DataAccess _dataAccess;
+
+    public CustomerBL()
+    {
+        _dataAccess = new DataAccess();
+    }
+
+    public String GetCustomerName(int id)
+    {
+        return _dataAccess.GetCustomerName(id);
+    }
+}
+
+class DataAccess //would be a new file in real app
+{
+    public DataAccess()
+    {
+    }
+
+    public String GetCustomerName(int id) {
+        return "Dummy Customer Name"; // get it from DB in real app
+    }
+}
